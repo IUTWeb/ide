1def104fc211707fb16dab0a5f09ea4a493a4f9f
Kayvan Mazaheri <KayvanMazaheri@GMail.com>
2017-12-23 17:51:05 +0330

    Initialize IDE Server Project

---
+++ server/.eslintrc.js
@@ -0,0 +1,18 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    es6: true,
+    node: true,
+  },
+  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+  extends: 'standard',
+  // add your custom rules here
+  rules: {
+
+  }
+}
